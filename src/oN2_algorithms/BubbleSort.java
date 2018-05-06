cac31d2dc1c5b0974f8cedbab8ed00a257c9929e
Smeesh <rahman.sameeha@gmail.com>
2018-05-06 12:07:59 +0530

    Added Selection Sort

--- src/oN2_algorithms/BubbleSort.java
+++ src/oN2_algorithms/BubbleSort.java
@@ -5,23 +5,26 @@ import java.util.Arrays;
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		
-		// this sorting algorithm guarantees at each iteration that the last element of the array is sorted.		
+
+		// this sorting algorithm guarantees at each iteration that the last element of
+		// the array is sorted.
 		int numbers[] = { 6, 0, 3, 5 };
 		boolean swapped = false;
-		
+
 		// the number of elements that still need to be sorted.
 		int numToSort = numbers.length - 1;
 
+		System.out.println(Arrays.toString(numbers));
 		do {
 			// to check if any swaps have occurred during this traversal of the array.
-			// if no swaps were made, the array is in order, and does not need to be checked again.
+			// if no swaps were made, the array is in order, and does not need to be checked
+			// again.
 			swapped = false;
 			for (int i = 0; i < numToSort; i++) {
-				
-				// if the smaller number is in the right of the array, 
+
+				// if the smaller number is in the right of the array,
 				if (numbers[i] > numbers[i + 1]) {
-					
+
 					// swap with the element before on the left.
 					int temp = numbers[i + 1];
 					numbers[i + 1] = numbers[i];
--- src/oN2_algorithms/InsertionSort.java
+++ src/oN2_algorithms/InsertionSort.java
@@ -6,13 +6,16 @@ public class InsertionSort {
 
 	public static void main(String[] args) {
 		int numbers[] = { 6, 0, 3, 5 };
+
+		System.out.println(Arrays.toString(numbers));
 		for (int i = 1; i < numbers.length; i++) {
-			
+
 			// get the first element from the unsorted side of the array.
 			int currentElement = numbers[i];
 			int j = i - 1;
-			
-			// check if the element in the unsorted side of the array is smaller than the largest element of the sorted one
+
+			// check if the element in the unsorted side of the array is smaller than the
+			// largest element of the sorted one
 			// if it is, then move the largest sorted element to the right of the array.
 			// repeat until you find the element less than the current unsorted element
 			while (j >= 0 && numbers[j] > currentElement) {
